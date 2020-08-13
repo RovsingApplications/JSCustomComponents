4cf63f5bd1be87b66c371ce34a0f906f1052d504
Omar-ABDELNABY <timonquint@yahoo.com>
2020-08-13 13:29:55 +0200

    custom elements: remove js-polyfills dependancy Causing A LOT of console errors

--- custom-input-elements/src/index.ts
+++ custom-input-elements/src/index.ts
@@ -1,4 +1,3 @@
-import 'js-polyfills';
 import '@webcomponents/webcomponentsjs/webcomponents-bundle';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
